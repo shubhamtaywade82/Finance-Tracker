27db77a2dab487b7443ffc017a196b7571089b9d
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-12 15:51:14 +0530

    stock lookup

---
+++ app/models/stock.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue StandardError => e
+      nil
+    end
+  end
+end
