9d82937259a1ff40df4d20b57353344b709822fa
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-12 14:12:05 +0530

    add logiut

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@
 
 Rails.application.routes.draw do
   root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
